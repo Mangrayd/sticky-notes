0a708828b513071aa0cf43c8541fdd24979e0274
FunnyM <Vadim.Pilyaev@cadfem-cis.ru>
2020-08-07 10:18:38 +0300

    Updates

---
+++ precache-manifest.a658b8225fecbb6e0355596ad83999c6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a782b854ca2bed2dade102bb5d7cba0",
+    "url": "/sticky-notes/index.html"
+  },
+  {
+    "revision": "0756cfba93323a508132",
+    "url": "/sticky-notes/static/css/main.8ae53a68.chunk.css"
+  },
+  {
+    "revision": "cead862c5751b4124501",
+    "url": "/sticky-notes/static/js/2.a284891a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sticky-notes/static/js/2.a284891a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0756cfba93323a508132",
+    "url": "/sticky-notes/static/js/main.b4002320.chunk.js"
+  },
+  {
+    "revision": "4b3830aa69bac6716be5",
+    "url": "/sticky-notes/static/js/runtime-main.cb183f1a.js"
+  },
+  {
+    "revision": "554f3a2bb5e80eaeb1ac9c2ffb6cac58",
+    "url": "/sticky-notes/static/media/drag.554f3a2b.svg"
+  },
+  {
+    "revision": "f49911d5029c29daf1305c89283285c8",
+    "url": "/sticky-notes/static/media/pattern.f49911d5.jpg"
+  },
+  {
+    "revision": "b610640205a1d91851d1976ece2b4318",
+    "url": "/sticky-notes/static/media/plus.b6106402.svg"
+  },
+  {
+    "revision": "eef58ba557d62efdfce140f0055b00d7",
+    "url": "/sticky-notes/static/media/trash.eef58ba5.svg"
+  }
+]);
